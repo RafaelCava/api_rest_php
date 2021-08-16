ec0c9f18d08ee1211cc9f76dc98905281a2928ef
Rafael <ra.facavalcante@hotmail.com>
2021-08-15 21:16:21 -0300

    index: inserindo lógica delete

--- index.php
+++ index.php
@@ -13,12 +13,29 @@
       // PODEMOS CONTINUAR NA API! TEMOS ACESSO
 
       if(isset($_GET['acao'])){
+        $pdo = new PDO('mysql:host=localhost;dbname=api_curso','root','');
         $acao = $_GET['acao'];
 
         if($acao == 'novo_contato'){
-          die(json_encode(array('sucesso'=>true)));
+
+          $nome = isset($_GET['nome']) ? $_GET['nome'] : '';
+
+          $sql = $pdo->prepare('INSERT INTO `clientes` VALUES (null,?)');
+          if($sql->execute(array($nome))){
+            die(json_encode(array('sucesso'=>true, 'inserido'=>$nome)));
+          }else{
+            die(json_encode(array('sucesso'=>false, 'erro'=>'não foi possível inserir seu contato.')));
+          }
+
         }else if($acao == 'deletar_contato'){
-          
+          if(!isset($_GET['id']))
+            die(json_encode(array('erro'=>'Precisamos de um id.')));
+          $id = (int)$_GET['id'];
+
+          $pdo->exec("DELETE FROM `clientes` WHERE id = $id");
+
+          die(json_encode(array('sucesso'=>true, 'deletado'=>$id)));
+
         }else if($acao == 'atualizar_contato'){
 
         }else if($acao == 'visualizar_contato'){
