ef0219e815bcb4938741f4ae58907196374f07b0
Rafael <ra.facavalcante@hotmail.com>
2021-08-15 21:22:42 -0300

    index: inserindo lógica visualizar contato

--- index.php
+++ index.php
@@ -10,8 +10,6 @@
   if(isset($_GET['token'])){
     $token = $_GET['token'];
     if($token == TOKEN){
-      // PODEMOS CONTINUAR NA API! TEMOS ACESSO
-
       if(isset($_GET['acao'])){
         $pdo = new PDO('mysql:host=localhost;dbname=api_curso','root','');
         $acao = $_GET['acao'];
@@ -31,19 +29,32 @@
           if(!isset($_GET['id']))
             die(json_encode(array('erro'=>'Precisamos de um id.')));
           $id = (int)$_GET['id'];
-
+          
           $pdo->exec("DELETE FROM `clientes` WHERE id = $id");
-
+          
           die(json_encode(array('sucesso'=>true, 'deletado'=>$id)));
-
+          
         }else if($acao == 'atualizar_contato'){
-
+          
         }else if($acao == 'visualizar_contato'){
+          if(!isset($_GET['id']))
+            die(json_encode(array('erro'=>'Precisamos de um id.')));
+          $id = (int)$_GET['id'];
+          
+          $sql = $pdo->prepare("SELECT * FROM `clientes` WHERE `id` = ?");
+          $sql->execute(array($id));
+
+          if($sql->rowCount()>=1){
+            $dados = $sql->fetch();
+            die(json_encode($dados));
+          }else{
+            die('Não encontramos nenhum usuário com esse id.');
+          }
 
         }else{
           die('A ação especificada não é valida em nosso sistema de API.');
         }
-
+        
 
       }else{
         die('Você não pode conectar na API sem uma ação definida');
